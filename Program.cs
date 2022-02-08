438899fb218fb95fc937ebf3834774acb50dd99d
Alexander <alek.agapow80@yandex.ru>
2022-02-08 17:42:57 +0700

    задача выполнена

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// 7.	Выяснить является ли число чётным
+
+Console.WriteLine("Введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num %2 == 0)
+
+System.Console.WriteLine($"Число {num} является четным");
+else System.Console.WriteLine($"Число {num} является  не четным");
+
+Console.ReadLine();
